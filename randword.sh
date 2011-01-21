851a8f17ad7d002e51c8ddcad0295c620dfff947
Art Witczak <art@contextoptional.com>
2011-01-21 10:38:04 -0800

    adding randword

---
+++ randword.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+WORDS=/usr/share/dict/words
+
+LINES=`wc -l $WORDS | awk '{ print ($1 + 1) }'`
+while :;
+do
+  RANDSEED=`date '+%S%M%I'`
+  LINE=`cat $WORDS | awk -v COUNT=$LINES -v SEED=$RANDSEED 'BEGIN { srand(SEED); \
+  i=int(rand()*COUNT) } FNR==i { print $0 }'`
+  echo "";echo "";echo "";echo ""; echo ""
+  figlet -c -w 132 -f slant $LINE
+  sleep 2
+  clear
+done
