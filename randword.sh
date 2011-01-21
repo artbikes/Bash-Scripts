c489fed8d37662e5d7cbbaf522afa65e7533c226
Art Witczak <art@contextoptional.com>
2011-01-21 10:41:53 -0800

    Moved clear to top of loop

--- randword.sh
+++ randword.sh
@@ -5,11 +5,11 @@ WORDS=/usr/share/dict/words
 LINES=`wc -l $WORDS | awk '{ print ($1 + 1) }'`
 while :;
 do
+  clear
   RANDSEED=`date '+%S%M%I'`
   LINE=`cat $WORDS | awk -v COUNT=$LINES -v SEED=$RANDSEED 'BEGIN { srand(SEED); \
   i=int(rand()*COUNT) } FNR==i { print $0 }'`
   echo "";echo "";echo "";echo ""; echo ""
   figlet -c -w 132 -f slant $LINE
   sleep 2
-  clear
 done
